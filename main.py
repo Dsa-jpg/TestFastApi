d91f55f57d7a4fc99df46a4c6b32a76689656fab
Filip <65063262+Dsa-jpg@users.noreply.github.com>
2024-08-11 20:10:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from typing import Optional
+from typing import Union
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
 
 
 @app.get("/")
@@ -12,3 +22,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
