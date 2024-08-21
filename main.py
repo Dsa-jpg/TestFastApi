39b4749ab8868519e9068b1556d32857348f3e26
Filip <65063262+Dsa-jpg@users.noreply.github.com>
2024-08-21 11:55:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ async def end_conversation():
     )
 
     # Opravený přístup k obsahu odpovědi
-    summarized_text = summary_response.choices[0].messages['content']
+    summarized_text = summary_response['choices'][0]['message']['content']
 
     # Vytvoříme konečný výstup pro uložení
     final_summary = {
